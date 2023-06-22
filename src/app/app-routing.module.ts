891c29eecff5fa404baba39df0bd5feb491546fa
azeez <azeez72835@gmail.com>
2023-06-22 17:23:15 +0530

    az

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { FirstComponent } from './first/first.component';
+import { LicenseComponent } from './license/license.component';
+import { MembersComponent } from './members/members.component';
+import { TemplateComponent } from './template/template.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home", component:FirstComponent
+  },
+  {
+    path:"license", component:LicenseComponent
+  },
+  {
+    path:"template", component:TemplateComponent
+  },
+  {
+    path:"contact", component:ContactComponent
+  },
+  {
+    path:"members", component:MembersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FirstComponent } from './first/first.component';
+import {FormsModule} from '@angular/forms';
+import { TemplateComponent } from './template/template.component';
+import { LicenseComponent } from './license/license.component';
+import { ContactComponent } from './contact/contact.component';
+import { MembersComponent } from './members/members.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    TemplateComponent,
+    LicenseComponent,
+    ContactComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
